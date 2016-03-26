dcdb34b8e45d0cd9e6dd01ca88765461b6fb4240
Justin Sternberg <justin@dsgnwrks.pro>
2016-03-26 19:24:03 -0400

    Add copyright block and credit block

--- loader.php
+++ loader.php
@@ -4,14 +4,38 @@
  *
  * Handles checking for and smartly loading the newest version of this library.
  *
- * @category WordPressLibrary
- * @package  LIBCLASSNAME
- * @author   AUTHORNAME <AUTHOREMAIL>
- * @license  GPL-2.0+
- * @version  0.1.0
- * @link     LIBURL
- * @since    0.1.0
+ * @category  WordPressLibrary
+ * @package   LIBCLASSNAME
+ * @author    AUTHORNAME <AUTHOREMAIL>
+ * @copyright 2016 AUTHORNAME <AUTHOREMAIL>
+ * @license   GPL-2.0+
+ * @version   0.1.0
+ * @link      LIBURL
+ * @since     0.1.0
  */
+
+/**
+ * Copyright (c) 2016 AUTHORNAME (email : AUTHOREMAIL)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Loader: https://github.com/jtsternberg/wp-lib-loader
+ */
+
 if ( ! class_exists( 'LIBCLASSNAME_010', false ) ) {
 
 	/**
