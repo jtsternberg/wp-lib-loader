13944212f919cab1e38b503e66dd313519a8ddb1
Justin Sternberg <justin@dsgnwrks.pro>
2016-03-28 16:57:53 -0400

    No more maintaining the readme in two places.. use JS lib to convert github flavored markdown

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,10 @@ window.WPLibLoader = window.WPLibLoader || {};
 ( function( window, document, $, app, undefined ) {
 	'use strict';
 
-	app.button = '<a href="https://raw.githubusercontent.com/jtsternberg/wp-lib-loader/master/loader.php" class="get-it button" download="loader.php">Generate Loader!</a>';
 	app.codeURL = 'https://raw.githubusercontent.com/jtsternberg/wp-lib-loader/master/loader.php?d=3-27-16';
+	app.button = '<a href="'+ app.codeURL +'" class="get-it button" download="loader.php">Generate Loader!</a>';
+	app.readmeURL = 'https://raw.githubusercontent.com/jtsternberg/wp-lib-loader/master/README.md?d=3-27-16';
+	app.readmeURL = 'http://test.it/Plugins&Scripts/wp-lib-loader/README.md';
 	app.code = '';
 
 	app.questions = {
@@ -16,31 +18,72 @@ window.WPLibLoader = window.WPLibLoader || {};
 		AUTHOREMAIL  : ['Library author email? (e.g. john@johndoe.com)', 'john@johndoe.com'],
 	};
 
-	app.init = function() {
-		app.$button = app.addButton();
-		app.getCode();
-
-		app.$button.on( 'click', app.generate );
+	app.readmeReplacements = {
+		'This is a handy template' : '<a href="https://github.com/jtsternberg/wp-lib-loader/blob/master/loader.php">WP Lib Loader</a> is a handy template',
+		'_WP<em>Magic</em>' : '<em>WP_Magic</em>'
 	};
 
-	app.addButton = function() {
+	app.init = function() {
+		// Get/parse readme contents
+		$.get( app.readmeURL, app.parseREADME );
+
+		// Get loader.php contents, and then add the generate-loader buttons
+		$.get( app.codeURL, app.addGeneratorButtons );
+
+		// Hook generate button
+		$( document.body ).on( 'click', '.get-it', app.clickGenerate );
+	};
+
+	app.parseREADME = function( mdContent ) {
+		var $mainContent = $( document.getElementById( 'main-content' ) );
+
+		// Process markdown
+		var html = marked( mdContent, {
+			highlight: function (code, lang, callback) {
+				// Use highlight.js to process code blocks
+				return hljs.highlightAuto( code, [ lang ] ).value;
+			}
+		} );
+
+		// Replace a few strings for the github page
+		html = app.replace( html, app.readmeReplacements );
+
+		// Generate a new jQuery dom object
+		var $html = $( '<div class="content-inner">' ).append( html );
+
+		// Remove title
+		$html.find( 'h1' ).first().remove();
+		// Remove github.io link
+		$html.find( 'h3' ).first().remove();
+		// Remove sub-title
+		$html.find( 'blockquote' ).first().remove();
+		// Remove "Use the generator tool to quickly create a loader for your library!"
+		$html.find( 'h4' ).first().remove();
+
+		// Replace link to this page with the generate loader button
+		$html.find( '[href="http://jtsternberg.github.io/wp-lib-loader/"]' )
+			.attr( 'href', app.codeURL )
+			.attr( 'download', 'loader.php' )
+			.addClass( 'get-it' )
+			.text( 'click the Generate Loader button' );
+
+		// Now replace the placeholder content w/ this markdown -> html content
+		$( document.getElementById( 'placeholder' ) ).replaceWith( $html );
+	};
+
+	app.addGeneratorButtons = function( data ) {
+		app.code = data;
 		$( 'header .inner' ).append( app.button );
 		$( '.generator-button' ).replaceWith( app.button );
-
-		return $( '.get-it' );
 	};
 
-	app.getCode = function() {
-		$.get( app.codeURL, function( data ) {
-			app.code = data;
-		} );
-	};
+	app.clickGenerate = function( evt ) {
+		evt.preventDefault();
 
-	app.generate = function( evt ) {
 		if ( ! app.code ) {
 			alert( 'Sorry, something went wrong! We couldn\'t download the template file.' );
 			app.getCode();
-			return evt.preventDefault();
+			return;
 		}
 
 		var replacements = {};
@@ -56,23 +99,17 @@ window.WPLibLoader = window.WPLibLoader || {};
 		} );
 
 		if ( ! replacements.LIBCLASSNAME || bail ) {
-			return evt.preventDefault();
+			return;
 		}
 
 		replacements.LIBNAMEUPPER = replacements.LIBCLASSNAME.toUpperCase();
 		replacements.LIBNAMELOWER = replacements.LIBCLASSNAME.toLowerCase();
 
-		var loader = app.code;
-		var regex;
+		var filename = replacements.LIBNAMELOWER.replace( '_', '-' ) + '.php';
 
-		$.each( replacements, function( replace, replacement ) {
-			regex = new RegExp( replace, 'gm');
-			loader = loader.replace( regex, replacement );
-		} );
+		app.download( filename, app.replace( app.code, replacements ) );
 
-		alert( 'Success! Your loader will now download.');
-
-		this.href = 'data:text/plain;charset=UTF-8,' + encodeURIComponent( loader );
+		alert( 'Success! Your loader (loader.php) has been downloaded.');
 	};
 
 	app.ask = function( replacements, replace, question ) {
@@ -87,6 +124,27 @@ window.WPLibLoader = window.WPLibLoader || {};
 		}
 
 		return true;
+	};
+
+	app.download = function( filename, text ) {
+		var element = document.createElement( 'a' );
+		element.setAttribute( 'href', 'data:application/octet-stream;charset=UTF-8,' + encodeURIComponent( text ) );
+		element.setAttribute( 'download', filename );
+
+		element.style.display = 'none';
+		document.body.appendChild( element );
+
+		element.click();
+
+		document.body.removeChild(element);
+	};
+
+	app.replace = function( content, replacements ) {
+		$.each( replacements, function( replace, replacement ) {
+			content = content.replace( new RegExp( replace, 'gm'), replacement );
+		} );
+
+		return content;
 	};
 
 	$( app.init );
